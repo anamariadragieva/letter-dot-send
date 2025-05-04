7f5bb3e5469f8cd4f99f4582cdfa76304a48fc1d
Ana Maria <anamariatxt@gmail.com>
2025-05-04 14:02:37 +0100

    keep only working features

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded( { extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("blogs.ejs", { blogs });
 });
 
 app.get("/blogs", (req, res) => {
